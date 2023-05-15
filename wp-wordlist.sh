9ad84398ffc366240e01872c74d0ef8b2f79de06
Security Sphinx <87793589+SecuritySphinx@users.noreply.github.com>
2023-05-15 16:45:14 +0530

    Update wp-wordlist.sh

--- wp-wordlist.sh
+++ wp-wordlist.sh
@@ -8,7 +8,7 @@ ENDCOLOR="\e[0m"
 # Help function
 help() {
     echo -e "${RED}Usage:${ENDCOLOR}"
-    echo -e "${GREEN}bash wp-wordlist.sh [option]${ENDCOLOR}"
+    echo -e "${GREEN}bash wp-wordlist.sh [option] [output_file]${ENDCOLOR}"
     echo -e "${RED}Options:${ENDCOLOR}"
     echo -e "${GREEN}-p, --plugin\tGenerate a wordlist of WordPress plugins${ENDCOLOR}"
     echo -e "${GREEN}-t, --theme\tGenerate a wordlist of WordPress themes${ENDCOLOR}"
@@ -18,11 +18,12 @@ help() {
 # Generate WordPress wordlist
 wp_wordlist() {
     option="$1"
+    output_file="$2"
 
     if [[ "$option" == "plugin" ]]; then
-        curl -s https://plugins.svn.wordpress.org/ | tail -n +5 | sed -e 's/<[^>]*>//g' -e 's/\///' -e 's/ \+//gp' | grep -v "Powered by Apache" | sort -u
+        curl -s https://plugins.svn.wordpress.org/ | tail -n +5 | sed -e 's/<[^>]*>//g' -e 's/\///' -e 's/ \+//gp' | grep -v "Powered by Apache" | sort -u > "$output_file"
     elif [[ "$option" == "theme" ]]; then
-        curl -s https://themes.svn.wordpress.org/ | tail -n +5 | sed -e 's/<[^>]*>//g' -e 's/\///' -e 's/ \+//gp' | grep -v "Powered by Apache" | sort -u
+        curl -s https://themes.svn.wordpress.org/ | tail -n +5 | sed -e 's/<[^>]*>//g' -e 's/\///' -e 's/ \+//gp' | grep -v "Powered by Apache" | sort -u > "$output_file"
     else
         echo -e "${RED}Invalid option.${ENDCOLOR}"
         help
@@ -41,11 +42,15 @@ while [[ $# -gt 0 ]]; do
 
     case $key in
         -p|--plugin)
-            wp_wordlist "plugin"
+            output_file="$2"
+            wp_wordlist "plugin" "$output_file"
+            echo -e "${GREEN}WordPress plugin wordlist generated successfully in '$output_file'.${ENDCOLOR}"
             exit 0
             ;;
         -t|--theme)
-            wp_wordlist "theme"
+            output_file="$2"
+            wp_wordlist "theme" "$output_file"
+            echo -e "${GREEN}WordPress theme wordlist generated successfully in '$output_file'.${ENDCOLOR}"
             exit 0
             ;;
         -h|--help)
